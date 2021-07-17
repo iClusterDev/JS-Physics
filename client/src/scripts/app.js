319730a940fbd0775e2a7f92959d8367319dcccf
iClusterDev <cluster.coredev@gmail.com>
2021-07-17 17:52:20 +0100

    store component interactions

--- client/src/scripts/app.js
+++ client/src/scripts/app.js
@@ -1,193 +1,171 @@
 import Store from './lib/Store';
-import Display from './lib/Display';
 import Engine from './lib/Engine';
-import Controller from './lib/Controller';
-import ECSEntity from './lib/ECSEntity';
-import ECSSystem from './lib/ECSSystem';
-import ECSComponent from './lib/ECSComponent';
+import Display from './lib/Display';
 
-// components setup
-const componentRect = new ECSComponent({
-  name: 'rect',
-  data: {
-    x: 0,
-    y: 0,
-    width: 0,
-    height: 0,
+/**
+ * store
+ */
+const game = new Store({
+  state: {
+    loading: true,
+    running: false,
   },
-});
 
-const componentPhysics = new ECSComponent({
-  name: 'physics',
-  data: {
-    vx: 0.0,
-    vy: 0.0,
+  actions: {
+    loading: (context, payload) => {
+      context.commit('loading', payload);
+    },
+
+    start: (context) => {
+      context.commit('start');
+    },
+
+    stop: (context) => {
+      context.commit('stop');
+    },
+
+    pause: (context) => {
+      context.commit('pause');
+    },
   },
-});
 
-const componentGraphics = new ECSComponent({
-  name: 'graphics',
-  data: {
-    colour: 'transparent',
-  },
-});
+  mutations: {
+    loading: (state, payload) => {
+      state.loading = payload;
+    },
 
-const componentInput = new ECSComponent({
-  name: 'input',
-  data: {
-    controller: new Controller([
-      { code: 'KeyA', action: 'moveLeft' },
-      { code: 'KeyD', action: 'moveRight' },
-    ]),
-  },
-});
-
-// entities setup
-const player = new ECSEntity({
-  name: 'player',
-  components: [
-    componentGraphics,
-    componentPhysics,
-    componentInput,
-    componentRect,
-  ],
-})
-  .setComponent('rect', {
-    x: 10,
-    y: 20,
-    width: 50,
-    height: 50,
-  })
-  .setComponent('physics', {
-    vx: 0.4,
-    vy: 0.4,
-  })
-  .setComponent('graphics', {
-    colour: 'red',
-  });
-
-const enemy1 = new ECSEntity({
-  name: 'enemy',
-  components: [componentGraphics, componentPhysics, componentRect],
-})
-  .setComponent('rect', {
-    x: 200,
-    y: 700,
-    width: 25,
-    height: 25,
-  })
-  .setComponent('physics', {
-    vx: 0.0,
-    vy: 0.7,
-  })
-  .setComponent('graphics', {
-    colour: 'black',
-  });
-
-const enemy2 = new ECSEntity({
-  name: 'enemy',
-  components: [componentGraphics, componentPhysics, componentRect],
-})
-  .setComponent('rect', {
-    x: 500,
-    y: 0,
-    width: 50,
-    height: 50,
-  })
-  .setComponent('physics', {
-    vx: 0.0,
-    vy: 0.8,
-  })
-  .setComponent('graphics', {
-    colour: 'blue',
-  });
-
-const entities = [player, enemy1, enemy2];
-
-// systems setup
-const systemInputMove = new ECSSystem({
-  name: 'systemMove',
-  onNext: (elapsedTime, entities) => {
-    entities.forEach((entity) => {
-      let { rect = null, physics = null, input = null } = entity.components;
-      if (rect && physics && input) {
-        let deltaX = 0;
-        if (input.controller.moveLeft.isActive)
-          deltaX = -physics.vx * elapsedTime;
-        if (input.controller.moveRight.isActive)
-          deltaX = physics.vx * elapsedTime;
-
-        rect.x += deltaX;
+    start: (state) => {
+      if (!state.loading && !state.running) {
+        state.running = true;
+        console.log('DEBUG ~ state.running', state.running);
       }
-    });
-  },
-});
+    },
 
-const systemBounce = new ECSSystem({
-  name: 'systemBounce',
-  onNext: (context, elapsedTime, entities) => {
-    entities.forEach((entity) => {
-      let { rect = null, physics = null, input = null } = entity.components;
-      if (rect && physics && !input) {
-        let deltaX = physics.vx * elapsedTime;
-        let deltaY = physics.vy * elapsedTime;
-        let newX = rect.x + deltaX;
-        let newY = rect.y + deltaY;
-
-        rect.x += deltaX;
-        rect.y += deltaY;
-
-        if (newX < 0) {
-          physics.vx = -physics.vx;
-          rect.x = 0;
-        } else if (newX + rect.width > context.canvas.width) {
-          physics.vx = -physics.vx;
-          rect.x = context.canvas.width - rect.width;
-        } else if (newY < 0) {
-          physics.vy = -physics.vy;
-          rect.y = 0;
-        } else if (newY + rect.height > context.canvas.height) {
-          physics.vy = -physics.vy;
-          rect.y = context.canvas.height - rect.height;
-        }
+    stop: (state) => {
+      if (!state.loading && state.running) {
+        state.running = false;
+        console.log('DEBUG ~ state.running', state.running);
       }
-    });
+    },
+
+    pause: (state) => {
+      if (!state.loading && state.running) {
+        state.running = false;
+        console.log('DEBUG ~ state.running', state.running);
+      }
+    },
   },
 });
 
-const systemRender = new ECSSystem({
-  name: 'systemRender',
-  onNext: (context, entities) => {
-    context.clearRect(0, 0, context.canvas.width, context.canvas.height);
-    entities.forEach((entity) => {
-      let { rect = null, graphics = null } = entity.components;
-      if (rect && graphics) {
-        context.fillStyle = graphics.colour;
-        context.fillRect(rect.x, rect.y, rect.width, rect.height);
-      }
+/**
+ * loading message
+ */
+class LoadingMessage {
+  #element;
+
+  constructor() {
+    this.#element = document.createElement('div');
+    this.#element.id = 'loadingMessage';
+    this.#element.classList.add('message');
+
+    this.#create();
+    this.#render();
+  }
+
+  #create() {
+    document.querySelector('#game').appendChild(this.#element);
+
+    let self = this;
+    game.on('loading-change', () => {
+      game.state.loading
+        ? self.#element.classList.toggle('hidden', false)
+        : self.#element.classList.toggle('hidden', true);
     });
-  },
-});
+  }
+
+  #render() {
+    this.#element.innerHTML = `<span class="message-text message-pulse">LOADING</span>`;
+  }
+}
+
+/**
+ * start message
+ */
+class StartMessage {
+  #element;
+
+  constructor() {
+    this.#element = document.createElement('div');
+    this.#element.id = 'startMessage';
+    this.#element.classList.add('message', 'hidden');
+
+    this.#create();
+    this.#render();
+  }
+
+  #create() {
+    document.querySelector('#game').appendChild(this.#element);
+
+    let self = this;
+    game.on('loading-change', () => {
+      game.state.loading
+        ? self.#element.classList.toggle('hidden', true)
+        : self.#element.classList.toggle('hidden', false);
+    });
+  }
+
+  #render() {
+    this.#element.innerHTML = `
+      <div class="message-text">START A NEW GAME</div>
+      <small class="message-text">PRESS SPACE</small>
+    `;
+  }
+}
+
+const loadGUI = () => {
+  new LoadingMessage();
+  new StartMessage();
+};
+
+const loadResources = () => {
+  setTimeout(() => {
+    game.dispatch('loading', false);
+  }, 2000);
+};
+
+const loadListeners = () => {
+  window.addEventListener('keydown', (e) => {
+    e.preventDefault();
+    switch (e.code) {
+      case 'Space':
+        game.dispatch('start');
+        break;
+      case 'Escape':
+        game.dispatch('stop');
+        break;
+      case 'KeyP':
+        game.dispatch('pause');
+        break;
+    }
+  });
+};
 
 export default () => {
-  // display setup
-  const display = new Display({
-    id: 'canvas',
-    width: 832,
-    height: 640,
-  });
-
-  // engine setup
-  const engine = new Engine(
-    (elapsedTime) => {
-      systemInputMove.next(elapsedTime, entities);
-      systemBounce.next(display.context, elapsedTime, entities);
-    },
-    () => {
-      systemRender.next(display.context, entities);
-    }
-  );
-
-  // start
-  engine.start();
+  loadGUI();
+  loadListeners();
+  loadResources();
+  // new StartButton();
+  // const display = new Display({
+  //   id: 'canvas',
+  //   width: 832,
+  //   height: 640,
+  //   color: 'cyan',
+  // });
+  // const engine = new Engine(
+  //   () => console.log(),
+  //   () => console.log('render')
+  // );
+  // game.on('isRunning-change', () => {
+  //   game.state.isReady && game.state.isRunning ? engine.start() : engine.stop();
+  // });
 };
